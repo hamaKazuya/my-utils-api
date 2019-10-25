4f6a3833db17ff0a4795035c1a21e71d3f5ac17f
hamaKazuya <haman.kazuya@gmail.com>
2019-10-25 16:51:20 +0900

    fix api for gorm.

--- cmd/main.go
+++ cmd/main.go
@@ -2,16 +2,16 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"my-utils-api/internal/model"
 	"my-utils-api/internal/package/db"
 	"net/http"
-	"strconv"
+	"reflect"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
-	"github.com/pkg/errors"
 )
 
 // User aaa
@@ -28,21 +28,14 @@ type Member struct {
 	Name string
 }
 
-// Todo struct
-// type Todo struct {
-// 	Id        int    `db:"id" json:"id"`
-// 	Title     string `db:"title" json:"title"`
-// 	IsDone    int    `db:"is_done" json:"isDone"`
-// 	Detail    string `db:"detail" json:"detail"`
-// 	CreatedAt string `db:"created_at" json:"createdAt"`
-// 	UpdatedAt string `db:"updated_at" json:"updatedAt"`
-// }
-
+// SctIsDone is ~~
+// TODO struct名が変
 type SctIsDone struct {
 	ID     int  `db:"id" json:"id"`
 	IsDone bool `db:"is_done" json:isDone`
 }
 
+// SctDeleteTodo is ~~
 type SctDeleteTodo struct {
 	ID int `db:"id" json:"id"`
 }
@@ -58,7 +51,6 @@ func main() {
 }
 
 func initRouting(e *echo.Echo) {
-	e.GET("/api/v1/member", getMember)
 	e.GET("/api/todo", getTodos)
 	e.GET("/api/todo/:todoId", getTodoByID)
 	e.POST("/api/todo/add", addTodo)
@@ -67,112 +59,32 @@ func initRouting(e *echo.Echo) {
 	e.POST("/api/todo/updateTodoByID", updateTodoByID)
 }
 
-func getUsers(c echo.Context) error {
-	groupIDstr := c.Param("group_id")
-	groupID, err := strconv.Atoi(groupIDstr)
-	if err != nil {
-		return errors.Wrapf(err, "errors when group id convert to int: %s", groupIDstr)
-	}
-	gender := c.QueryParam("gender")
-	users := []*User{}
-	if gender == "" || gender == "man" {
-		users = append(users, &User{ID: 1, GroupID: groupID, Name: "Taro", Gender: "man"})
-		users = append(users, &User{ID: 2, GroupID: groupID, Name: "Jiro", Gender: "man"})
-	}
-	if gender == "" || gender == "woman" {
-		users = append(users, &User{ID: 3, GroupID: groupID, Name: "hanako", Gender: "woman"})
-		users = append(users, &User{ID: 4, GroupID: groupID, Name: "Yoshiko", Gender: "woman"})
-	}
-	return c.JSON(http.StatusOK, users)
-}
-
-func getMember(c echo.Context) error {
-	db, err := sql.Open("mysql", "root@/todo")
-
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-	println("open ok.")
-	var id int
-	var name string
-	rows, err := db.Query("SELECT * FROM members")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	println(rows)
-
-	members := []*Member{}
-	for rows.Next() {
-		err := rows.Scan(&id, &name)
-		if err != nil {
-			log.Fatal(err)
-		}
-		println(id, name)
-		members = append(members, &Member{
-			ID:   id,
-			Name: name,
-		})
-	}
-	return c.JSON(http.StatusOK, members)
-}
-
-func getTodoByID(c echo.Context) error {
-	todoID := c.Param("todoId")
-	// println("todoId", todoId)
-
-	db, err := sql.Open("mysql", "root@/todo")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-	row, err := db.Query("SELECT * FROM todo_list WHERE id = ?", todoID)
-	if err != nil {
-		log.Fatal(err)
-	}
-	todos := []model.Todo{}
-	var (
-		id        int
-		title     string
-		isDone    int
-		detail    string
-		createdAt string
-		updatedAt string
-	)
-	for row.Next() {
-		err = row.Scan(
-			&id,
-			&title,
-			&isDone,
-			&detail,
-			&createdAt,
-			&updatedAt,
-		)
-		if err != nil {
-			log.Fatal(err)
-		}
-		todo := model.Todo{
-			ID:        id,
-			Title:     title,
-			IsDone:    isDone,
-			Detail:    detail,
-			CreatedAt: createdAt,
-			UpdatedAt: updatedAt,
-		}
-		todos = append(todos, todo)
-	}
-	return c.JSON(http.StatusOK, todos)
-}
-
 // FIXME errorの返り値これであってるのかな
 func getTodos(c echo.Context) error {
 	database := db.Connect()
 	defer database.Close()
 
 	todosEx := []model.Todo{}
+	// gormはtableの指定がない場合、構造体の複数形の名称でtableを参照する
 	database.Find(&todosEx)
 	return c.JSON(http.StatusOK, todosEx)
+}
+
+func getTodoByID(c echo.Context) error {
+	// c.Paramはstringらしい
+	paramID := c.Param("todoId")
+
+	database := db.Connect()
+	defer database.Close()
+
+	todoEx := model.Todo{}
+	println("paramID: ", paramID, reflect.TypeOf(paramID))
+	fmt.Printf("paramID: %T\n", paramID)
+
+	// 構造体の情報を元にテーブルを参照するらしい
+	todoEx.ID = 1 // FIXME paramIDがstringだけど構造体的にはintでcastしたいんだけどできない
+	result := database.First(&todoEx)
+	return c.JSON(http.StatusOK, result)
 }
 
 func addTodo(c echo.Context) (err error) {
@@ -186,19 +98,14 @@ func addTodo(c echo.Context) (err error) {
 
 	db := db.Connect()
 	defer db.Close()
-	// db, err := sql.Open("mysql", "root@/todo")
-	// println("addtodo")
-	// if err != nil {
-	// 	println("opennotoko")
-	// 	log.Fatal(err)
-	// }
-	// defer db.Close()
 
 	todoEx := model.Todo{}
 	todoEx.Title = title
 	todoEx.Detail = detail
 	todoEx.CreatedAt = "2013-11-17 21:34:10"
-	// TODO 未指定似できない
+	// FIXME
+	// 未指定にしてMySQL側で勝手にtimestampが入ってほしいんだけど、
+	// 構造体的にnilがだめだからとりあえず適当なdateを入れている
 	// Error 1292: Incorrect datetime value: '' for column 'updated_at' at row 1
 	// time.formatうんちゃらでYYYY-MM-DD HH:MM:SSにしたいんだけどもなぁ
 	todoEx.UpdatedAt = "2013-11-17 21:34:10"
@@ -206,31 +113,25 @@ func addTodo(c echo.Context) (err error) {
 	if result.Error != nil {
 		log.Fatal(result.Error)
 	}
-	// ins, err := db.Prepare("INSERT INTO todo_list (title, detail) VALUES (?, ?)")
-	// if err != nil {
-	// 	println("dbnotoko")
-	// 	log.Fatal(err)
-	// }
-	// ins.Exec(title, detail)
+
 	return getTodos(c)
 }
 
 func deleteTodoByID(c echo.Context) (err error) {
-	println("start deleteTodoByID")
-	t := new(SctDeleteTodo)
-	if err = c.Bind(t); err != nil {
-		return err
-	}
-	todoID := t.ID
+	params := SctDeleteTodo{}
 
-	db, err := sql.Open("mysql", "root@/todo")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
+	database := db.Connect()
+	defer database.Close()
 
-	dlt, err := db.Prepare("DELETE FROM todo_list WHERE id = ?;")
-	dlt.Exec(todoID)
+	todoEx := model.Todo{}
+	todoEx.ID = params.ID
+
+	// FIXME
+	// .firstをやらないとmodelにprimary_key追加しても全件削除になってしまう
+	// where句使ったほうが安心っていうしそうしたほうがいいのかな
+	database.First(&todoEx)
+	database.Delete(&todoEx)
+
 	return getTodos(c)
 }
 
